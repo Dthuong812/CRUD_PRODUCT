f63b3d1e6826fe97b7c707142ddfd7a58901696a
Dathuong812 <tranthidathuong812@gmail.com>
2024-12-30 12:35:56 +0700

    config router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from '../../todoApp/src/pages/ErrorPage';
+import Product from './pages/Product';
 
+const router = createBrowserRouter([{
+  path :"/",
+  element :<App></App>,
+  errorElement:<ErrorPage></ErrorPage>,
+  children:[
+    {
+      index : true,
+      element:<Product></Product>
+    }
+  ]
+
+}])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router= {router}/>
   </StrictMode>,
 )
